96f09d7df19aa40f016fcb5d13276214d2293a9b
elrobe <eliasroberg@gmail.com>
2023-01-30 18:56:18 -0600

    Solution for 2022 day 19 part 2

--- 2022/19/solution.go
+++ 2022/19/solution.go
@@ -15,6 +15,11 @@
 // you the most number of geodes, per blueprint. Given a "quality" number of the blueprint
 // id (blueprint #) multiplied by the max number of geodes the blueprint can collect in 24
 // minutes, what is the sum of every blueprint's quality number?
+//
+// Part 2 -
+// The elephants ate and now you actually have 32 minutes to crack geodes. Unfortunately,
+// part of the elephants' meal was most of the blueprints, so you only have the first three
+// blueprints left. What is the product of all three blueprints' max number of geodes?
 
 // To run:
 // - Make sure you have Go installed on your machine
@@ -25,23 +30,38 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"regexp"
 	"strconv"
 	"strings"
 )
 
+func maxOf(num ...int) int {
+	max := 0
+	for i := 0; i < len(num); i++ {
+		val := num[i]
+		if val > max {
+			max = val
+		}
+	}
+	return max
+}
+
 /**
  * Parses the given input file and returns the blueprints as an array of integer arrays.
  * Based on the input, each array should be exactly 7 in length, where each index
  * denotes the following:
  * 0 = Blueprint ID
+ * --------------------------------------
  * 1 = Ore-collecting robot ore cost
  * 2 = Clay-collecting robot ore cost
  * 3 = Obsidian-collecting robot ore cost
  * 4 = Obsidian-collecting robot clay cost
  * 5 = Geode-cracking robot ore cost
  * 6 = Geode-cracking robot obsidian cost
+ * --------------------------------------
+ * 7 = Max ore needed to create any robot (for path pruning)
  *
  * Indices 3 and 4 are needed to make an obsidian-collecting robot
  * Indices 5 and 6 are needed to make a geode-cracking robot
@@ -73,9 +93,10 @@ func parseInput(fileName string) [][]int {
 		// Not the prettiest way to write it
 		var blueprint []int
 		for m := 0; m < len(matches); m++ {
-			val, _ := strconv.Atoi(matches[m])
+			val, _ := strconv.Atoi(matches[m]) // We expect no errors here
 			blueprint = append(blueprint, val)
 		}
+		blueprint = append(blueprint, maxOf(blueprint[1], blueprint[2], blueprint[3], blueprint[5]))
 
 		blueprints = append(blueprints, blueprint)
 	}
@@ -146,27 +167,44 @@ func runRecurse(bp []int, robots uint32, rocks uint32, nextRobot int, max *int, 
 
 	// Base case: we've run out of minutes, so count up the geodes and calculate the quality level
 	if minutes == 1 {
-		id := bp[0]
 		geodes := getGeodes(newRocks)
-		if id*geodes > *max {
-			*max = id * geodes
+		if geodes > *max {
+			*max = geodes
 		}
 	} else {
-		// Now go through and recurse on each type of robot you can create
-		// Always try to create the most important robot...
+		minLeft := minutes - 1
+		// Let's try and prune this branch by seeing if the total number of Geodes we could
+		// still collect is less than the max we've seen by now
+		geoRobots := getGeodes(robots)
+		possibleGeodes := getGeodes(rocks) // Start the current number of geodes
+		for i := 0; i < minutes; i++ {
+			geoRobots++ // We can create one geode robot
+			possibleGeodes += geoRobots
+		}
+		if possibleGeodes < *max {
+			return
+		}
+
+		// Now go through and recurse on each type of robot you can create.
+		// Always try to create the most important robot, but also don't create
+		// any more robots than the max number of materials we need to create that robot
 		if canConstruct(3, bp, newRocks) {
-			runRecurse(bp, newRobots, consumeRocks(3, bp, newRocks), 3, max, minutes-1)
-		} else if canConstruct(2, bp, newRocks) {
-			runRecurse(bp, newRobots, consumeRocks(2, bp, newRocks), 2, max, minutes-1)
-		} else if canConstruct(1, bp, newRocks) {
-			runRecurse(bp, newRobots, consumeRocks(1, bp, newRocks), 1, max, minutes-1)
+			runRecurse(bp, newRobots, consumeRocks(3, bp, newRocks), 3, max, minLeft)
 		}
-		if canConstruct(0, bp, newRocks) {
-			runRecurse(bp, newRobots, consumeRocks(0, bp, newRocks), 0, max, minutes-1)
+		if canConstruct(2, bp, newRocks) && getObsidian(newRobots) < bp[6] {
+			runRecurse(bp, newRobots, consumeRocks(2, bp, newRocks), 2, max, minLeft)
 		}
-		runRecurse(bp, newRobots, newRocks, -1, max, minutes-1)
+		if canConstruct(1, bp, newRocks) && getClay(newRobots) < bp[4] {
+			runRecurse(bp, newRobots, consumeRocks(1, bp, newRocks), 1, max, minLeft)
+		}
+		if canConstruct(0, bp, newRocks) && getOre(newRobots) < bp[7] {
+			runRecurse(bp, newRobots, consumeRocks(0, bp, newRocks), 0, max, minLeft)
+		}
+		runRecurse(bp, newRobots, newRocks, -1, max, minLeft)
 	}
 }
+
+// Helper functions to parse the uint64 rocks/robots counts
 
 func getOre(rocks uint32) int {
 	return int(rocks & 0xFF)
@@ -187,36 +225,60 @@ func getGeodes(rocks uint32) int {
 /**
  * Kicks off the recursive algorithm with initial values.
  */
-func runBlueprint(bp []int) int {
+func runBlueprint(bp []int, time int, c chan int) {
 	var robots uint32
-	robots = 0x00000001
+	robots = 0x00000001 // Start with 1 ore-collecting robot
 	var rocks uint32
-	rocks = 0x00000000 // Always starts at 0
+	rocks = 0x00000000 // Always starts at 0 resources
 	max := 0
 
-	runRecurse(bp, robots, rocks, -1, &max, 24)
+	runRecurse(bp, robots, rocks, -1, &max, time)
 
-	return max
+	// The return value is dependent on part 1 vs. part 2
+	if time == 24 { // Part 1
+		c <- (max * bp[0])
+	} else { // Part 2
+		c <- max
+	}
 }
 
 /**
  * The main script being run here. Parses the input and outputs the solution value.
  */
 func main() {
-	// Step 1: Read the input file, parse it into Blueprints
+	// Read the input file, parse it into Blueprints
 	inputFile := "input.txt"
 	if len(os.Args) == 2 && os.Args[1] == "-sample" {
 		inputFile = "sample_input.txt"
 	}
 	blueprints := parseInput(inputFile)
 
+	// Set up the multithreading!
+	c := make(chan int)
+
 	// Now go through each blueprint and create a run!
+	//start := time.Now()
 	sum := 0
 	for b := 0; b < len(blueprints); b++ {
-		runVal := runBlueprint(blueprints[b])
-		fmt.Printf("BP#%d gets %d geodes\n", b+1, runVal)
-		sum += runVal
+		go runBlueprint(blueprints[b], 24, c)
 	}
-
+	for b := 0; b < len(blueprints); b++ {
+		sum += <-c
+	}
 	fmt.Printf("Part 1: %d\n", sum)
+	//fmt.Printf("(Ran in %fs)\n", time.Since(start).Seconds())
+
+	// Part 2 is the same as part 1, but we only have the first three blueprints,
+	// but we have 32 minutes to collect
+	//start = time.Now()
+	runs := int(math.Min(3, float64(len(blueprints))))
+	product := 1
+	for b := 0; b < runs; b++ {
+		go runBlueprint(blueprints[b], 32, c)
+	}
+	for b := 0; b < runs; b++ {
+		product *= <-c
+	}
+	fmt.Printf("Part 2: %d\n", product)
+	//fmt.Printf("(Ran in %fs)\n", time.Since(start).Seconds())
 }

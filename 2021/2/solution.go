adbb39f0b838faaefc48a307bf6810f9fc04dc79
elrobe <eliasroberg@gmail.com>
2023-07-28 00:24:59 -0400

    Solution for 2021 Day 2 Part 2

--- 2021/2/solution.go
+++ 2021/2/solution.go
@@ -14,6 +14,52 @@ func now() int64 {
 	return nanos / 1000000
 }
 
+func part1(lines []string) int {
+	x := 0 // Progress in the X-direction
+	y := 0 // Progress in the Y-direction
+	for i := 0; i < len(lines); i++ {
+		line := lines[i]
+		commands := strings.Split(line, " ")
+		dir := commands[0]
+		dist, _ := strconv.Atoi(commands[1])
+
+		if dir == "forward" {
+			x += dist
+		} else if dir == "up" {
+			y -= dist
+		} else {
+			// Must be "down"
+			y += dist
+		}
+	}
+	return x * y
+}
+
+func part2(lines []string) int {
+	// All things considered, I could just process part 2 as a part of the
+	// loop in part 1, but we'll just copy and paste to keep it cleaner.
+	x := 0
+	y := 0
+	a := 0 // 'a' is short for "aim" here
+	for i := 0; i < len(lines); i++ {
+		line := lines[i]
+		commands := strings.Split(line, " ")
+		dir := commands[0]
+		dist, _ := strconv.Atoi(commands[1])
+
+		if dir == "up" {
+			a -= dist
+		} else if dir == "down" {
+			a += dist
+		} else {
+			// Must be "forward"
+			x += dist
+			y += a * dist
+		}
+	}
+	return x * y
+}
+
 func main() {
 	inputFile := "input.txt"
 	if len(os.Args) == 2 && os.Args[1] == "-sample" {
@@ -29,24 +75,9 @@ func main() {
 	dataStr := string(data[:])
 	lines := strings.Split(dataStr, "\n")
 
-	x := 0 // Progress in the X-direction
-	y := 0 // Progress in the Y-direction
 	start := now()
-	for i := 0; i < len(lines); i++ {
-		line := lines[i]
-		commands := strings.Split(line, " ")
-		dir := commands[0]
-		dist, _ := strconv.Atoi(commands[1])
+	fmt.Printf("Part 1: %d (%dms)\n", part1(lines), now()-start)
 
-		if dir == "forward" {
-			x += dist
-		} else if dir == "up" {
-			y -= dist
-		} else {
-			// Must be "down"
-			y += dist
-		}
-
-	}
-	fmt.Printf("Part 1: %d (%dms)\n", x*y, now()-start)
+	start = now()
+	fmt.Printf("Part 2: %d (%dms)\n", part2(lines), now()-start)
 }

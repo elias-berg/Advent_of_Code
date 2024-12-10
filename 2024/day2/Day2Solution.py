dd23cc0a1eb3b72cad238e7df4f3f10940980070
elrobe <eliasroberg@gmail.com>
2024-12-09 19:47:39 -0500

    2024 Day 2 Part 2

--- 2024/day2/Day2Solution.py
+++ 2024/day2/Day2Solution.py
@@ -4,22 +4,43 @@ class Day2Solution(Solution):
   def __init__(self):
     super().__init__(2)
 
+  def isReportSafe(self, report):
+    diff = 0
+    for i in range(0, len(report) - 1):
+      newDiff = report[i] - report[i+1]
+      if abs(newDiff) < 1 or abs(newDiff) > 3 or (newDiff < 0 and diff > 0) or (newDiff > 0 and diff < 0):
+        return False
+      diff = newDiff
+    return True
+
   def part1(self):
     lines = self.readInput()
 
     cnt = 0
     for line in lines:
       nums = list(map(int, line.split(" ")))
-      diff = 0
-      safe = True # Whether or not we hit the end of the sequence; assume True, set to False ASAP
-      for i in range(0, len(nums) - 1):
-        newDiff = nums[i] - nums[i+1]
-        if abs(newDiff) < 1 or abs(newDiff) > 3 or (newDiff < 0 and diff > 0) or (newDiff > 0 and diff < 0):
-          safe = False
-          break
-        diff = newDiff
-      if safe:
+      if self.isReportSafe(nums):
         cnt = cnt + 1
+    return cnt
+  
+  def part2(self):
+    lines = self.readInput()
+
+    cnt = 0
+    for line in lines:
+      nums = list(map(int, line.split(" ")))
+      if self.isReportSafe(nums):
+        cnt = cnt + 1
+      # Basically just take part 1 and run it back for the list without individual values missing
+      else:
+        size = len(nums)
+        for i in range(0, len(nums)):
+          nums2 = nums[0:i] + nums[i+1:size]
+          if self.isReportSafe(nums2):
+            cnt = cnt + 1
+            break
+
+
     return cnt
   
   pass
--- 2024/day2/urls.py
+++ 2024/day2/urls.py
@@ -2,5 +2,6 @@ from .Day2Solution import Day2Solution
 
 Day2 = Day2Solution()
 Day2.part1Done = True
+Day2.part2Done = True
 
 urlpatterns = Day2.urls()

c2262ef8cb66e4c93f73acca4b990453cdb7ab89
elrobe <eliasroberg@gmail.com>
2024-12-10 20:33:46 -0500

    2024 Day 4 Part 2

--- 2024/day4/solution.py
+++ 2024/day4/solution.py
@@ -1,5 +1,7 @@
 from template.Solution import Solution
 
+# Recursive check for Part 1 to see if XMAS is spelled in a particular
+# direction dictated by the `func`
 def check(grid, y, x, iter, func):
   [newX, newY] = func(x, y)
   if newX < 0 or newX == len(grid[0]) or newY < 0 or newY == len(grid):
@@ -13,10 +15,22 @@ def check(grid, y, x, iter, func):
     return 1
   return 0
 
+# Check for Part 2 to see if an "A" is diagonally crossed by M and S in one direction.
+# It takes two checks to thus confirm, using the alternate diagonal for the second check.
+def check2(grid, y, x, func):
+  [newX1, newY1, newX2, newY2] = func(x, y)
+  # Bounds-check
+  if newX1 < 0 or newX1 == len(grid[0]) or newY1 < 0 or newY1 == len(grid) or \
+     newX2 < 0 or newX2 == len(grid[0]) or newY2 < 0 or newY2 == len(grid):
+    return False
+  return (grid[newY1][newX1] == "S" and grid[newY2][newX2] == "M") or \
+    (grid[newY1][newX1] == "M" and grid[newY2][newX2] == "S")
+
 class Day4Solution(Solution):
   def __init__(self):
     super().__init__(4)
     self.part1 = True
+    self.part2 = True
 
   def Part1(self):
     grid = self.readInput()
@@ -32,6 +46,20 @@ class Day4Solution(Solution):
           cnt = cnt + check(grid, y, x, 0, lambda x, y: [x + 1, y]) # Right
           cnt = cnt + check(grid, y, x, 0, lambda x, y: [x + 1, y - 1]) # Diag Up Right
           cnt = cnt + check(grid, y, x, 0, lambda x, y: [x + 1, y + 1]) # Diag Down Right
+    return cnt
+
+  def Part2(self):
+    grid = self.readInput()
+    cnt = 0
+    # Same strategy as part 1, except key off of the letter 'A' instead
+    # and then check the diagonals
+    for y in range(0, len(grid)):
+      for x in range(0, len(grid[y])):
+        if grid[y][x] == "A" and \
+          check2(grid, y, x, lambda x, y: [x + 1, y + 1, x - 1, y - 1]) and \
+          check2(grid, y, x, lambda x, y: [x - 1, y + 1, x + 1, y - 1]):
+          cnt = cnt + 1
+
     return cnt
   
   pass

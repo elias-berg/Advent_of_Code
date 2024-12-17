82bc3cd0c505ed243211fb16c68b5e57bd9e9d4a
elrobe <eliasroberg@gmail.com>
2024-12-16 23:48:11 -0500

    2024 Day 13 Part 2

--- 2024/day13/solution.py
+++ 2024/day13/solution.py
@@ -44,29 +44,63 @@ def tryPresses(a: Button, b: Button, p: Prize, tokens: int, x: int, y: int, aPre
   memo[key] = minTokens
   return minTokens
   
-
 class Day13Solution(Solution):
   def __init__(self):
     super().__init__(13)
     self.part1 = True
+    self.part2 = True
 
   # This is pretty slow because it's absolutely exhaustive, despite memoization.
   # TODO: Figure out how to speed it up...
   def Part1(self):
     input = self.readInput()
+    input.append("")
     # Read-loop
     tokens = 0
     while len(input) > 0:
-      a = input.pop(0)
-      if a == "": # In case of empty row
-        a = input.pop(0)
-      a = Button(a, 3)
+      a = Button(input.pop(0), 3)
       b = Button(input.pop(0), 1)
       prize = Prize(input.pop(0))
+      _ = input.pop(0) # Empty line
 
       minTokens = tryPresses(a, b, prize, 0, 0, 0, 0, 0, {})
       if minTokens < sys.maxsize:
         tokens += minTokens
+    return tokens
+  
+  # Add 10000000000000 to the Prize X and Y...
+  def Part2(self):
+    input = self.readInput()
+    input.append("")
+    # Read-loop
+    tokens = 0
+    while len(input) > 0:
+      a = Button(input.pop(0), 3)
+      b = Button(input.pop(0), 1)
+      p = Prize(input.pop(0))
+      _ = input.pop(0) # Empty line
+      p.x += 10_000_000_000_000
+      p.y += 10_000_000_000_000
+
+      # Here goes Eli's attempt at linear algebra, oh boy!
+      # The approach was to use a1x + b1y = c1, then solve for x (note: a1 * x).
+      # Then, use that and plug it in for x to then solve for y in a2x + b2y = c2.
+      # So you end up with:
+      # y = (a1*c2 - a2*c1) / (a1*b2 - a2*b1)
+      #
+      # Only if we get a whole number can we get a minimum number!
+      denominator = (a.x * b.y) - (a.y * b.x)
+      if not denominator == 0: # Dividing by 0 would be BAD
+        numerator = (a.x * p.y) - (a.y * p.x)
+        [bPresses, remainder] = divmod(numerator, denominator)
+        if remainder == 0: # No partial presses!
+          # We're making an assumption here that a.x is > 0 or else we'd hit a divide by 0 error
+          # solving for x, since x = (c1 - b1y) / a1x
+          numerator2 = (p.x - (b.x * bPresses))
+          [aPresses, remainder] = divmod(numerator2, a.x)
+          if remainder == 0: # No partial presses!
+            print(f"A Presses: {aPresses}, B Presses: {bPresses}")
+            tokens += (a.cost * aPresses) + (b.cost * bPresses)
     return tokens
 
   pass
